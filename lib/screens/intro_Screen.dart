9329f078afaffa7e5e831f1581aeeea5bc8f98f2
JohnGastone <johngastone11@gmail.com>
2023-07-27 10:03:54 +0300

    Fonts++

--- lib/screens/intro_Screen.dart
+++ lib/screens/intro_Screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:dashboard/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intro_screen_onboarding_flutter/intro_app.dart';
 
 void main() {
@@ -29,23 +30,31 @@ class TestScreen extends StatelessWidget {
   final List<Introduction> list = [
     Introduction(
       title: 'Uendeshaji',
-      subTitle: 'Fuatilia ujazo wa vinywaji mbalimbali',
+      subTitle: 'Fuatilia ujazaji wa vinywaji mbalimbali',
       imageUrl: 'images/pic/bottle.png',
+      titleTextStyle: GoogleFonts.lato(fontSize: 35),
+      subTitleTextStyle: GoogleFonts.lato(fontSize: 20),
     ),
     Introduction(
       title: 'Taarifa',
       subTitle: 'Tengeneza Ripoti za kazi',
       imageUrl: 'images/pic/reports.png',
+      titleTextStyle: GoogleFonts.lato(fontSize: 35),
+      subTitleTextStyle: GoogleFonts.lato(fontSize: 20),
     ),
     Introduction(
       title: 'Pendekeza',
       subTitle: 'Shauri maboresho ya huduma',
       imageUrl: 'images/pic/reward.png',
+      titleTextStyle: GoogleFonts.lato(fontSize: 35),
+      subTitleTextStyle: GoogleFonts.lato(fontSize: 20),
     ),
     Introduction(
       title: 'Data',
       subTitle: 'Tuma data kwa urahisi',
       imageUrl: 'images/pic/data.png',
+      titleTextStyle: GoogleFonts.lato(fontSize: 35),
+      subTitleTextStyle: GoogleFonts.lato(fontSize: 20),
     ),
   ];
 
