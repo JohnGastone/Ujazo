98581bb1be0058a7d59413067207fa92a7e29079
JohnGastone <johngastone11@gmail.com>
2024-03-13 09:51:31 +0300

    ++

--- lib/widgets/vinywaji_grid.dart
+++ lib/widgets/vinywaji_grid.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:dashboard/data/data.dart';
+import 'package:dashboard/screens/kazi_zilizopita.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,55 +18,61 @@ class KinywajiGrid extends StatelessWidget {
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             childAspectRatio: 16 / 7, crossAxisCount: 1, mainAxisSpacing: 20),
         itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(kinywaji[index].backImage),
-                  fit: BoxFit.fill),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        kinywaji[index].name,
-                        style: GoogleFonts.spaceMono(
-                            color: Colors.white, fontSize: 18),
-                      ),
-                      Text(
-                        kinywaji[index].amount,
-                        style: GoogleFonts.spaceMono(color: Colors.white),
-                      ),
-                      CircularPercentIndicator(
-                        radius: 30,
-                        lineWidth: 8,
-                        animation: true,
-                        animationDuration: 1500,
-                        circularStrokeCap: CircularStrokeCap.round,
-                        percent: kinywaji[index].percent / 100,
-                        progressColor: Colors.white,
-                        center: Text(
-                          "${kinywaji[index].percent}%",
+          return InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => KaziZilizopita()));
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(kinywaji[index].backImage),
+                    fit: BoxFit.fill),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          kinywaji[index].name,
+                          style: GoogleFonts.spaceMono(
+                              color: Colors.white, fontSize: 18),
+                        ),
+                        Text(
+                          kinywaji[index].amount,
                           style: GoogleFonts.spaceMono(color: Colors.white),
                         ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        kinywaji[index].imageUrl,
-                        height: 110,
-                      )
-                    ],
-                  )
-                ],
+                        CircularPercentIndicator(
+                          radius: 30,
+                          lineWidth: 8,
+                          animation: true,
+                          animationDuration: 1500,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          percent: kinywaji[index].percent / 100,
+                          progressColor: Colors.white,
+                          center: Text(
+                            "${kinywaji[index].percent}%",
+                            style: GoogleFonts.spaceMono(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          kinywaji[index].imageUrl,
+                          height: 110,
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           );
